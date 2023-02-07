bb45eb385ae7e3158953676e55a508f43ba90890
William Dan <william04dan@gmail.com>
2023-02-06 21:56:08 -0500

    Make the dragging more robust.

--- main-page-cover/script.js
+++ main-page-cover/script.js
@@ -1,67 +1,32 @@
 const cover = document.getElementById("cover")
 
 cover.addEventListener('mousedown', (e) => {
+    if (cover.dataset.startPosition === '-2') return;
+    console.log('mousedown');
+
     cover.dataset.startPosition = e.clientX;
     cover.style.transitionProperty = `none`;
 });
 
 cover.addEventListener('mousemove', (e) => {
-    if (cover.dataset.startPosition === '-1') return;
+    if (cover.dataset.startPosition === '-1' || cover.dataset.startPosition === '2') return;
 
     const mouseDelta = parseFloat(cover.dataset.startPosition) - e.clientX;
     const maxDelta = window.innerWidth;
 
-    const dragPercentage = Math.min((mouseDelta / maxDelta) * -100, 0);
-    cover.dataset.currentPercentage = parseFloat(cover.dataset.startPercentage) + dragPercentage;
-    cover.style.transform = `translateX(${cover.dataset.currentPercentage}%)`;
+    const dragPercentage = Math.min((mouseDelta/ maxDelta) * -100, 0);
+    cover.style.transform = `translateX(${dragPercentage}%)`;
 });
 
 cover.addEventListener('mouseup', () => {
-    cover.dataset.startPosition = '-1';
+    if (cover.dataset.startPosition === '-1') return;
+
+    cover.dataset.startPosition = -2;
     cover.style.transitionProperty = `transform`;
     cover.style.transform = `translateX(0%)`;
 });
 
-cover.addEventListener('transitioncancel', (e) => {
-    cover.dataset.startPercentage = parseFloat(cover.dataset.currentPercentage) * (1 - e.elapsedTime);
-    console.log(cover.dataset.startPercentage);
-});
-
 cover.addEventListener('transitionend', () => {
-    cover.dataset.startPercentage = 0;
+    console.log('transition end');
+    cover.dataset.startPosition = -1;
 });
-
-// cover.addEventListener('mousedown', (e) => {
-//     cover.dataset.mouseDownAt = e.clientX;
-//     cover.style.transitionProperty = `none`;
-// });
-
-// cover.addEventListener('mousemove', (e) => {
-//     if (cover.dataset.mouseDownAt === '0') return;
-
-//     const mouseDelta = parseFloat(cover.dataset.mouseDownAt) - e.clientX;
-//     const maxDelta = window.innerWidth;
-
-//     const percentage = Math.min((mouseDelta / maxDelta) * -100, 0);
-    
-//     cover.dataset.currentPercentage = percentage
-//     cover.style.transform = `translateX(${(percentage)}%)`;
-// });
-
-// cover.addEventListener('mouseup', () => {
-//     cover.dataset.mouseDownAt = '0';
-//     cover.style.transitionProperty = `transform`;
-//     if (parseFloat(cover.dataset.currentPercentage) < -70) {
-//         cover.style.transform = `translateX(-100%)`;
-//     } else {
-//         cover.style.transform = `translateX(0%)`;
-//     }
-// });
-
-// cover.addEventListener('transitionend', () => {
-//     cover.dataset.currentPercentage = '0';
-// });
-
-// cover.addEventListener('transitioncancel', (e) => {
-//     cover.dataset.currentPercentage *= e.elapsedTime;
-// });
