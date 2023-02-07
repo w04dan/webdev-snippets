57d4161c0a468ba046078f5591e8975129ec41c2
William Dan <william04dan@gmail.com>
2023-02-06 21:20:41 -0500

    Add more robust dragging. Specifically add handling for reclick during transition.

--- main-page-cover/script.js
+++ main-page-cover/script.js
@@ -1,29 +1,67 @@
 const cover = document.getElementById("cover")
 
-window.onmousedown = e => {
-    cover.dataset.mouseDownAt = e.clientX;
+cover.addEventListener('mousedown', (e) => {
+    cover.dataset.startPosition = e.clientX;
     cover.style.transitionProperty = `none`;
-}
+});
 
-window.onmousemove = e => {
-    if (cover.dataset.mouseDownAt === "0") return;
+cover.addEventListener('mousemove', (e) => {
+    if (cover.dataset.startPosition === '-1') return;
 
-    const mouseDelta = parseFloat(cover.dataset.mouseDownAt) - e.clientX;
+    const mouseDelta = parseFloat(cover.dataset.startPosition) - e.clientX;
     const maxDelta = window.innerWidth;
 
-    const percentage = Math.min((mouseDelta / maxDelta) * -100, 0);
-    
-    cover.dataset.currentPercentage = percentage
-    cover.style.transform = `translateX(${(percentage)}%)`;
-}
+    const dragPercentage = Math.min((mouseDelta / maxDelta) * -100, 0);
+    cover.dataset.currentPercentage = parseFloat(cover.dataset.startPercentage) + dragPercentage;
+    cover.style.transform = `translateX(${cover.dataset.currentPercentage}%)`;
+});
 
-window.onmouseup = () => {
-    cover.dataset.mouseDownAt = "0";
+cover.addEventListener('mouseup', () => {
+    cover.dataset.startPosition = '-1';
     cover.style.transitionProperty = `transform`;
-    if (parseFloat(cover.dataset.currentPercentage) < -50) {
-        cover.style.transform = `translateX(-100%)`;
-    } else {
-        cover.style.transform = `translateX(0%)`;
-    }
-    cover.dataset.currentPercentage = "0";
-}
+    cover.style.transform = `translateX(0%)`;
+});
+
+cover.addEventListener('transitioncancel', (e) => {
+    cover.dataset.startPercentage = parseFloat(cover.dataset.currentPercentage) * (1 - e.elapsedTime);
+    console.log(cover.dataset.startPercentage);
+});
+
+cover.addEventListener('transitionend', () => {
+    cover.dataset.startPercentage = 0;
+});
+
+// cover.addEventListener('mousedown', (e) => {
+//     cover.dataset.mouseDownAt = e.clientX;
+//     cover.style.transitionProperty = `none`;
+// });
+
+// cover.addEventListener('mousemove', (e) => {
+//     if (cover.dataset.mouseDownAt === '0') return;
+
+//     const mouseDelta = parseFloat(cover.dataset.mouseDownAt) - e.clientX;
+//     const maxDelta = window.innerWidth;
+
+//     const percentage = Math.min((mouseDelta / maxDelta) * -100, 0);
+    
+//     cover.dataset.currentPercentage = percentage
+//     cover.style.transform = `translateX(${(percentage)}%)`;
+// });
+
+// cover.addEventListener('mouseup', () => {
+//     cover.dataset.mouseDownAt = '0';
+//     cover.style.transitionProperty = `transform`;
+//     if (parseFloat(cover.dataset.currentPercentage) < -70) {
+//         cover.style.transform = `translateX(-100%)`;
+//     } else {
+//         cover.style.transform = `translateX(0%)`;
+//     }
+// });
+
+// cover.addEventListener('transitionend', () => {
+//     cover.dataset.currentPercentage = '0';
+// });
+
+// cover.addEventListener('transitioncancel', (e) => {
+//     cover.dataset.currentPercentage *= e.elapsedTime;
+// });
