ad223642115daf55f3fd30738fa01a3f4f846177
William Dan <william04dan@gmail.com>
2023-02-06 23:05:46 -0500

    Add finishing.

--- main-page-cover/script.js
+++ main-page-cover/script.js
@@ -2,31 +2,36 @@ const cover = document.getElementById("cover")
 
 cover.addEventListener('mousedown', (e) => {
     if (cover.dataset.startPosition === '-2') return;
-    console.log('mousedown');
 
+    cover.dataset.dragPercentage = 0;
     cover.dataset.startPosition = e.clientX;
     cover.style.transitionProperty = `none`;
 });
 
 cover.addEventListener('mousemove', (e) => {
-    if (cover.dataset.startPosition === '-1' || cover.dataset.startPosition === '2') return;
+    if (cover.dataset.startPosition === '-1' || cover.dataset.startPosition === '-2') return;
 
     const mouseDelta = parseFloat(cover.dataset.startPosition) - e.clientX;
     const maxDelta = window.innerWidth;
 
-    const dragPercentage = Math.min((mouseDelta/ maxDelta) * -100, 0);
-    cover.style.transform = `translateX(${dragPercentage}%)`;
+    cover.dataset.dragPercentage = Math.min((mouseDelta/ maxDelta) * -100, 0);
+    cover.style.transform = `translateX(${cover.dataset.dragPercentage}%)`;
+
+    if (parseFloat(cover.dataset.dragPercentage) < -60) {
+        cover.dataset.startPosition = '-2';
+        cover.style.transitionProperty = `transform`;
+        cover.style.transform = `translateX(-100%)`;
+    }
 });
 
 cover.addEventListener('mouseup', () => {
-    if (cover.dataset.startPosition === '-1') return;
+    if (cover.dataset.startPosition === '-1' || cover.dataset.startPosition === '-2') return;
 
-    cover.dataset.startPosition = -2;
+    cover.dataset.dragPercentage === '0' ? cover.dataset.startPosition = -1 : cover.dataset.startPosition = -2;
     cover.style.transitionProperty = `transform`;
     cover.style.transform = `translateX(0%)`;
 });
 
 cover.addEventListener('transitionend', () => {
-    console.log('transition end');
     cover.dataset.startPosition = -1;
 });
